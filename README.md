a34d914b7b2f291b60b0a3ed8b60a15699dadac2
Wadaken <kentaro.w215@gmail.com>
2023-04-23 06:36:50 +0900

    new commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # YOLOv3
 YOLOv3 model built using Pytorch.
+
+# model.py
+yolov3のネットワークアーキテクチャを構築する。
+
+# create_anchor.py
+k-Meansクラスタリングを行い、AnchorBox9つを出力させる。
+
+# dataset.py
+YOLOv3のbbox予測アルゴリズムを実装。
+学習するためのデータセットをPytorchで構築する。
--- dataset.py
+++ dataset.py
@@ -8,11 +8,7 @@ from torchvision.ops.boxes import box_iou
 import glob
 import os
 import torch
-#import torch.nn as nn
 import create_anchor 
-#import pandas as pd
-
-
 
 class YOLOv3_Dataset(Dataset):
   
@@ -28,8 +24,7 @@ class YOLOv3_Dataset(Dataset):
     self.anchor_iou = torch.cat([torch.zeros(9,2) , torch.tensor(self.anchor_dict[["width","height"]].values)] ,dim = 1)
 
 
-
-#coco128からラベルを読み出す
+#coco128からラベルをリスト型で読み出す
   def get_label(self , path): 
     bbox_list = []
     with open(path , 'r',newline='\n') as f:  
@@ -49,24 +44,24 @@ class YOLOv3_Dataset(Dataset):
       aw = anchor["width"] #aw,ahはアンカーボックスのサイズ
       ah = anchor["height"]
       twth.append([math.log(wh[0]/aw) , math.log(wh[1]/ah)])
+      #print(type(twth))
       #print(twth)
     return twth
 
   # 中心座標をCx,Cy,tx,tyに変換する関数(YOLOv3の予測tx,ty)
   def cxcy2txty(self,cxcy):
     #全ての出力サイズに対して計算している。特定のサイズのみで十分
-    map_size = [int(self.img_size/32) , int(self.img_size/16) , int(self.img_size/8)] ##why(32,16,8)
+    map_size = [int(self.img_size/32) , int(self.img_size/16) , int(self.img_size/8)] 
     txty = []
     for size in map_size:
       #grid_x,grid_y:bboxの中心座標(0~1)*それぞれの出力サイズ=それぞれの出力サイズにおける1ピクセルの幅Cx,Cy
       grid_x = int(cxcy[0]*size) ###int
       grid_y = int(cxcy[1]*size)
       
-      #
       tx = math.log((cxcy[0]*size - grid_x + 1e-10) / (1 - cxcy[0]*size +grid_x+ 1e-10))
       ty = math.log((cxcy[1]*size - grid_y+ 1e-10) / (1 - cxcy[1]*size + grid_y+ 1e-10))
       txty.append([grid_x , tx , grid_y ,ty])
-      #print(txty)
+
     return txty
 
   #ラベルをテンソルに変換する関数
@@ -78,43 +73,29 @@ class YOLOv3_Dataset(Dataset):
     for size in map_size:
       for x in range(3):
         tensor_list.append(torch.zeros((4 + 1 + self.class_n,size,size))) 
+        #print(tensor_list)
+        #print(tensor_list[0].shape)
     
     ##yolov4の予測結果をリストに格納
-    for bbox in bbox_list: 
+    for bbox in bbox_list: #bbox_listはcoco128データセットの中のlabelsのリスト
       cls_n = int(bbox[0])
-      txty_list = self.cxcy2txty(bbox[1:3]) #bboxの中心座標
-      twth_list = self.wh2twth(bbox[3:]) #bboxの幅と高さ
-      #print(bbox[3:])
-
+      txty_list = self.cxcy2txty(bbox[1:3]) #bboxの中心座標 一回転で3つのリスト
+      twth_list = self.wh2twth(bbox[3:]) #bboxの幅と高さ 一回転で9つのリスト
+      #if (bbox ==[0.0, 0.0884609, 0.379012, 0.0463594, 0.112612] ):
+        #print(txty_list)
+      #print(bbox)
+      
       #バウンディングボックスと最も形状が近いAnchorBoxはIoUを用いて計算する。
       label_iou = torch.cat([torch.zeros((1,2))  , torch.tensor(bbox[3:]).unsqueeze(0)],dim=1) 
       iou = box_iou(label_iou, self.anchor_iou)[0]
-      obj_idx = torch.argmax(iou).item()
-      
-      """
-      #iou計算の結果の確認
-      if bbox == [26.0, 0.174266, 0.664047, 0.0525, 0.119341]:
-        print("bboxのtensor:\n",label_iou)
-        print("anchor_boxのtensor:\n", self.anchor_iou)
-        print("iou:\n",iou)
-        print("iouが最も大きいindex番号:\n",obj_idx)
-      """
+      obj_idx = torch.argmax(iou).item() #最もiouが大きかったindex番号 1回転で1つだけ出力
 
-      for i , twth in enumerate(twth_list): #iに入るのは0から8まで twthに入るのはyolov4の出力
+      for i , twth in enumerate(twth_list): #iに入るのは0から8まで twthに入るのはyolov4の出力(リスト)
         tensor = tensor_list[i] 
-        #print(tensor_list[1].size())
+        #print(tensor_list[3].size())
         txty = txty_list[int(i/3)] ##why box3つずつの中心点は変わらないから
-        """
-        #twthとtxtyの確認
-        if bbox == [26.0, 0.174266, 0.664047, 0.0525, 0.119341]:
-          print("index番号:",i)
-          print("yolov3の予測(幅と高さのずれ):\n",twth)
-          print("yolov3の予測(中心点のx座標とy座標のずれ):\n",txty)
-          print("tensorリスト:\n",tensor.size())
-          print("yolov3の予測(中心点のx座標とy座標のずれ)[1]:\n",txty[1])
-          print("yolov3の予測(中心点のx座標とy座標のずれ)[3]:\n",txty[3])
-        """
-        if i == obj_idx:    
+  
+        if i == obj_idx:  #iouが最も大きかった場合,yolov4の予測をtensorに追加する。
           tensor[0,txty[2],txty[0]] = txty[1] #tx
           tensor[1,txty[2],txty[0]] = txty[3] #ty
           tensor[2,txty[2],txty[0]] = twth[0] #th
@@ -127,20 +108,23 @@ class YOLOv3_Dataset(Dataset):
     scale1_label = torch.cat(tensor_list[6:] , dim = 0)
     #print("scale1_labelの次元数\n",scale1_label.dim())
     #print("scale1_labelのサイズ\n",scale1_label.size())
-
     return scale3_label , scale2_label , scale1_label  
 
   #__getitem__メソッド：指定されたインデックスに対応するデータとラベルを返すためのメソッド
   def __getitem__(self , idx):
     img_path = self.img_list[idx]
-    
     label_path = self.label_list[idx]
-    
-    bbox_list = self.get_label(label_path) #coco128データセットの中のlabels
+    #print(label_list[1])
+    bbox_list = self.get_label(label_path) #coco128データセットの中のlabelsのリスト
+    #画像を読み込んで、
     img = cv2.imread(img_path)
     img = cv2.resize(img , (self.img_size , self.img_size))
+    #print(img.shape)
     img = Image.fromarray(img)
+    #print(type(img)) PIL.Image.image
     img = self.transform(img)
+    #print(type(img)) torch.Tensor
+    #print(img.shape)
     scale3_label , scale2_label , scale1_label = self.label2tensor(bbox_list)
     
     return img , scale3_label , scale2_label , scale1_label
@@ -155,10 +139,12 @@ import numpy as np
 def visualization(y_pred,anchor,img_size,conf = 0.5,is_label = False):
   size = y_pred.shape[2]
   anchor_size = anchor[anchor["type"] == size]
+  #print(anchor_size)
   bbox_list = []
   for i in range(3):
-    a = anchor_size.iloc[i]
-    grid = img_size/size
+    a = anchor_size.iloc[i] ##指定されたインデックス番号の値を抽出
+    #print(a)
+    grid = img_size/size ##
     y_pred_cut = y_pred[0,i*(4 + 1 + class_n) :(i+1)*(4 + 1 + class_n) ].cpu()
     if is_label:
       y_pred_conf = y_pred_cut[4,:,:].cpu().numpy()
@@ -182,40 +168,52 @@ def show(imgs):
     fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
     for i, img in enumerate(imgs):
         img = img.detach()
-        img = FF.to_pil_image(img)
+        img = FF.to_pil_image(img) #tensor型からPILイメージに変換
         axs[0, i].imshow(np.asarray(img))
         axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 
 
 #メイン
-from torchvision.io import read_image
+#from torchvision.io import read_image
 from torchvision.utils import draw_bounding_boxes
 import matplotlib.pyplot as plt
 img_list = glob.glob(os.path.join("/home/wada/YOLOv3/coco128/images/train2017","*"))
 img_size = 416
-
 
 #print(anchor)
 import torchvision.transforms as T
 class_n = 80
 img_list = sorted(glob.glob(os.path.join("/home/wada/YOLOv3/coco128/images/train2017","*")))
 label_list = sorted(glob.glob(os.path.join("/home/wada/YOLOv3/coco128/labels/train2017","*")))
-transform = T.Compose([T.ToTensor()])
-train_data = YOLOv3_Dataset(img_list,label_list,80,img_size ,create_anchor.anchor,transform)
+transform = T.Compose([T.ToTensor()]) #tensorに変更、T.composeは複数のtransformを同時に行うときに使う。
+"""
+Datasetにおいて「def __len__()」と「def __getitem__()」は必須。
+Transform(dataの前処理)によるerrorを確認するため。
+transformによるerrorは参照して初めて出力される。
+Transformの処理に不安があるならば必ず事前に参照出力させておくとそこでエラー確認ができる
+"""
+train_data = YOLOv3_Dataset(img_list,label_list,80,img_size ,create_anchor.anchor,transform) 
+print(len(train_data))
+print(train_data)
+#第一引数：Dataset
 train_loader = DataLoader(train_data , batch_size = 1)
 
-import matplotlib.pyplot as plt
-plt.rcParams['figure.max_open_warning'] = 200
+##plt.rcParams['figure.max_open_warning'] = 200
 
 for n , (img , scale3_label , scale2_label ,scale1_label) in enumerate(train_loader):
   path = img_list[n]
+  #print(path)
   img = cv2.imread(path)[:,:,::-1]
+  #img = cv2.imread("/home/wada/YOLOv3/coco128/images/train2017/000000000650.jpg")
+  #print(img.shape())
   img = cv2.resize(img , (img_size , img_size))
   img = torch.tensor(img.transpose(2,0,1))
   preds = [scale3_label , scale2_label , scale1_label]
+  #スケールごとに色を変える
   for color,pred in zip(["red","green","blue"],preds):
     bbox_list = visualization(pred,create_anchor.anchor,img_size,conf = 0.9,is_label = True)
     img = draw_bounding_boxes(img, torch.tensor(bbox_list), colors=color, width=1)
   show(img)  
   #image = read_image(img)
   #plt.imshow(image)
+
